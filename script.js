580be2ec43ceef2dddefe7f629bcc2420bb1e095
goncalotbh <oliveira2gd@gmail.com>
2025-11-24 10:03:58 +0000

    WIP update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const GlobalLateralOffset = 30;
 let boxCounter = 0;
-let chatOpen = false;
+let chatBoxActive = false;
+let linkBoxActive = false;
+let galleryBoxActive = false;
+let thoughtsBoxActive = false;
 
 // ------------------ DRAG FUNCTION ------------------
 
@@ -123,18 +126,131 @@ function createLinksBox() {
   bodyDiv.appendChild(socialsList);
 
   newBox.append(titleDiv, bodyDiv);
+  newBox.style.zIndex = boxCounter;
   mainContainer.appendChild(newBox);
 
   dragElement(newBox);
 
+  flipStatus(linkBoxActive);
+
   console.log("Created new Links/Socials box with ID:", newBox.id);
+}
+
+// ------------------ CREATE THOUGHTS BOX ------------------
+
+function createThoughtsBox() {
+  const mainContainer = document.querySelector(".main");
+  const newBox = document.createElement("div");
+  newBox.className = "box";
+  newBox.id = "thoughtBox" + ++boxCounter;
+
+  // Title
+  const titleDiv = document.createElement("div");
+  titleDiv.className = "title";
+  titleDiv.id = newBox.id + "Header";
+
+  const titleP = document.createElement("p");
+  titleP.className = "title";
+  titleP.textContent = "Thoughts";
+
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "X";
+  closeBtn.onclick = () => newBox.remove();
+
+  const helpBtn = document.createElement("button");
+  helpBtn.textContent = "?";
+
+  titleDiv.append(titleP, closeBtn, helpBtn);
+
+  // Body
+  const bodyDiv = document.createElement("div");
+  bodyDiv.className = "body";
+
+  const wip = document.createTextNode("WIP");
+
+  bodyDiv.appendChild(wip);
+
+  newBox.append(titleDiv, bodyDiv);
+  newBox.style.zIndex = boxCounter;
+  mainContainer.appendChild(newBox);
+
+  dragElement(newBox);
+
+  flipStatus(thoughtsBoxActive);
+
+  console.log("Created new Thoughts box with ID:", newBox.id);
+}
+
+// ------------------ CREATE GALLERY BOX ------------------
+
+function createThoughtsBox() {
+  const mainContainer = document.querySelector(".main");
+  const newBox = document.createElement("div");
+  newBox.className = "box";
+  newBox.id = "galleryBox" + ++boxCounter;
+
+  // Title
+  const titleDiv = document.createElement("div");
+  titleDiv.className = "title";
+  titleDiv.id = newBox.id + "Header";
+
+  const titleP = document.createElement("p");
+  titleP.className = "title";
+  titleP.textContent = "Gallery";
+
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "X";
+  closeBtn.onclick = () => newBox.remove();
+
+  const helpBtn = document.createElement("button");
+  helpBtn.textContent = "?";
+
+  titleDiv.append(titleP, closeBtn, helpBtn);
+
+  // Body
+  const bodyDiv = document.createElement("div");
+  bodyDiv.className = "body";
+
+  const wip = document.createTextNode("WIP");
+
+  bodyDiv.appendChild(wip);
+
+  newBox.append(titleDiv, bodyDiv);
+  newBox.style.zIndex = boxCounter;
+  mainContainer.appendChild(newBox);
+
+  dragElement(newBox);
+
+  flipStatus(galleryBoxActive);
+
+  console.log("Created new Gallery box with ID:", newBox.id);
 }
 
 // ------------------ REMOVE ELEMENT ------------------
 
-function removeElmnt(id) {
+export function removeElmnt(id) {
   const element = document.getElementById(id);
   if (element) element.remove();
+}
+
+// ------------------ Box Status ----------------------
+
+export function getActiveStatus(boxString) {
+  switch(boxString) {
+    case "chat": return chatBoxActive;
+    case "links": return linkBoxActive;
+    case "gallery": return galleryBoxActive;
+    case "thoughts": return thoughtsBoxActive;
+  }
+}
+
+export function flipStatus(boxString) {
+  switch(boxString) {
+    case "chat": chatBoxActive = !chatBoxActive;
+    case "links": linkBoxActive = !linkBoxActive;
+    case "gallery": galleryBoxActive = !galleryBoxActive;
+    case "thoughts": thoughtsBoxActive = !thoughtsBoxActive;
+  }
 }
 
 // ------------------ INITIALIZE DRAG ON EXISTING BOXES ------------------
